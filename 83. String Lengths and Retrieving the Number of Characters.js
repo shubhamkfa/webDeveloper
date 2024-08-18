aba5e06eb6f1bc3fe59d6253b12a2f9a2f8aca27
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-18 16:26:18 +0530

    Update 83. String Lengths and Retrieving the Number of Characters.js

--- 83. String Lengths and Retrieving the Number of Characters.js
+++ 83. String Lengths and Retrieving the Number of Characters.js
@@ -1,1 +1,13 @@
 
+// alert("Type Text here")
+// window.alert("type you msg ")
+
+// window.close()
+
+
+let para = prompt("Enter your Paragraph Here")
+let input=para.length
+let limit= 280 
+
+
+console.log("You have written "+ input + " characters, you have "+ (limit-input) + " characters left."  )
