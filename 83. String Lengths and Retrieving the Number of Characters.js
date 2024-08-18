2cdd9aa59412224908669dd2fae50671ccd213e3
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-18 16:29:36 +0530

    backtick concatenate

--- 83. String Lengths and Retrieving the Number of Characters.js
+++ 83. String Lengths and Retrieving the Number of Characters.js
@@ -1,4 +1,3 @@
-
 // alert("Type Text here")
 // window.alert("type you msg ")
 
@@ -9,5 +8,8 @@ let para = prompt("Enter your Paragraph Here")
 let input=para.length
 let limit= 280 
 
+//string concatinate
+// console.log("You have written "+ input + " characters, you have "+ (limit-input) + " characters left."  )
 
-console.log("You have written "+ input + " characters, you have "+ (limit-input) + " characters left."  )
+//backtick concatinate
+console.log(`You have written ${input} characters, you have ${limit-input} characters left.`  )
