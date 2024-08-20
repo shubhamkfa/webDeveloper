9a7b261b930cb40b9c35b188dc1d9af5e01d82a7
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-20 23:40:56 +0530

    Update 83. String Lengths and Retrieving the Number of Characters.js

--- 83. String Lengths and Retrieving the Number of Characters.js
+++ 83. String Lengths and Retrieving the Number of Characters.js
@@ -1,16 +1,33 @@
-// alert("Type Text here")
-// window.alert("type you msg ")
+let name = prompt ("What is your name?")
+// alert(`Hello ${name.toUpperCase()}, How are you ? `)
 
-// window.close()
+// console.log(name.toUpperCase())
 
+//chalenge first letter capital only 
 
-let para = prompt("Enter your Paragraph Here")
-let input=para.length
-let limit= 280 
+let a= name.slice(0,1)
+let a1= a.toUpperCase()
+let b = name.slice(1,100)
+let final= a1+b
+// console.log(final)
 
-//string concetinate
-// console.log("You have written "+ input + " characters, you have "+ (limit-input) + " characters left."  )
+let second =name.split(" ")
+let var1=(second[0])
+let varf=(var1.slice(0,1))
+let u1=varf.toUpperCase()
+let var2=var1.slice(1,100)
+let finalzero=u1+var2
+// console.log(finalzero)
 
-//backtick concetinate//
-console.log(`You have written ${input} characters, you have ${limit-input} characters left.`  )
+let sar1=(second[1])
+let sarf=(sar1.slice(0,1))
+let s1=sarf.toUpperCase()
+let sar2=sar1.slice(1,100)
+let finalSecond=s1+sar2
+// console.log(finalSecond)
 
+let result= finalzero + " "+ finalSecond
+
+// alert(`Hello ${final}, How are you ? `)
+
+alert(`Hello ${result}, How are you ? `)
