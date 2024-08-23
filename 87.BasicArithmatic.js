fe35a89ca1c5bb657bdf78a89e56901f25dfe625
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-23 18:13:26 +0530

    Create 87.BasicArithmatic.js

---
+++ 87.BasicArithmatic.js
@@ -0,0 +1,17 @@
+// addition
+
+console.log(5+6);
+
+//substrction
+console.log(6-8);
+// Multiplication *
+// division /
+
+
+
+// Modulo - this is an usefull to check odd or even number 
+//modulo 0 = even
+// 1 = odd
+
+let num = 12%8 
+console.log(num);
